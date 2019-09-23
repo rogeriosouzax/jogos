b54f0f0a262c4934aa5f07b71b7f467fee65f380
Rogerio <rogeriosouzax@hotmail.com>
2019-09-22 21:00:55 -0300

    Adição da funcionalidade de carregar o nome da fruta pelo arquivo

--- forca.py
+++ forca.py
@@ -1,3 +1,4 @@
+from random import randrange
 
 def jogar():
 
@@ -5,38 +6,49 @@ def jogar():
     print('***Bem vindo ao jogo da forca***')
     print('********************************')
 
-    palavra_secreta = "banana"
-    palavra_display = []
+    with open("frutas", mode="r") as fruta:
+        frutas = fruta.read().split("\n")
+
+    index = randrange(0, len(frutas))
+
+    palavra_secreta = frutas[index]
+    palavra_display = ["_ " for letra in palavra_secreta]
+    erros = 0
 
     enforcou = False
     acertou = False
 
-    for c in range(0, len(palavra_secreta)):
-        palavra_display.append("_ ")
-
     print("".join(palavra_display))
 
-    while(not enforcou and not acertou):
+    while not enforcou and not acertou:
 
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(letra.lower() == chute.lower()):
-                palavra_display[index] = chute.lower()
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if letra == chute:
+                    palavra_display[index] = chute
+                index += 1
+        else:
+            erros += 1
 
         palavra = "".join(palavra_display)
         print("".join(palavra_display))
+        print("Você errou {}/5 vezes...".format(erros))
 
-        if(palavra_secreta != palavra):
+        if palavra_secreta != palavra and erros < 5:
             print("jogando...")
+        elif erros == 5:
+            print("Você perdeu!")
+            enforcou = True
         else:
-            print("Você acertou!")
-            break
+            print("Você venceu!")
+            acertou = True
+
 
 print("Fim do Jogo!")
 
-if(__name__ == '__main__'):
+if __name__ == "__main__":
     jogar()
